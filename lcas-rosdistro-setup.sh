1874cc360e9ace201e5b7c3b6ce2ff6a9ea7789c
Sergi Molina <smolinamellado@lincoln.ac.uk>
2021-11-17 09:39:35 +0000

    add gazebo repo

--- lcas-rosdistro-setup.sh
+++ lcas-rosdistro-setup.sh
@@ -53,6 +53,10 @@ fi
 
 source ~/.bashrc
 
+# ADD GAZEBO REPO
+$SUDO sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+$SUDO wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+
 # LCAS REPO CONFIG
 
 # Dependencies
